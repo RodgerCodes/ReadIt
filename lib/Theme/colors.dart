fac26012ada701b6948f40c340f06b04de30f3ba
RodgerCodes <rkumwanje@gmail.com>
2024-11-24 00:31:07 +0200

    chore: splash screen

---
+++ lib/Theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static Color loaderColor = const Color.fromARGB(255, 0, 140, 255);
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:readit/Theme/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,8 +12,30 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("I am splash screeen"),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                "assets/readit.png",
+                width: size.width * 0.2,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SpinKitFadingCircle(
+              color: AppColors.loaderColor,
+              size: 50,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
