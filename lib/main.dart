6bfa9db6700f078b3f9b16ee3729b819ec45a23e
RodgerCodes <rkumwanje@gmail.com>
2024-11-24 20:31:01 +0200

    chore: make it default dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:readit/router/index.dart';
 
 void main() {
@@ -6,6 +7,10 @@ void main() {
   runApp(
     FluentApp.router(
       routerConfig: router,
+      theme: FluentThemeData(
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        brightness: Brightness.dark,
+      ),
     ),
   );
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:readit/Theme/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,10 +11,70 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home"),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          // header
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Your Books",
+                style: TextStyle(
+                  fontSize: size.width * 0.02,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // TextFormField(
+              //   validator: (value) {
+              //     if (value == '') {
+              //       return 'Please enter your question title';
+              //     }
+
+              //     return null;
+              //   },
+              //   // keyboardType: TextInputType.,
+              //   enableSuggestions: true,
+              //   // maxLines: 2,
+              //   // controller: questionTitle,
+              //   decoration: InputDecoration(
+              //     isDense: true,
+              //     hintText: "Search",
+              //     labelText: "Question Title (brief)",
+              //     fillColor: Colors.white,
+              //     enabledBorder: const OutlineInputBorder(
+              //       borderSide: BorderSide(
+              //         color: Colors.black,
+              //         width: 1.0,
+              //       ),
+              //     ),
+              //     focusedErrorBorder: const OutlineInputBorder(
+              //       borderSide: BorderSide(
+              //         color: Colors.red,
+              //         width: 2,
+              //       ),
+              //     ),
+              //     focusedBorder: OutlineInputBorder(
+              //       borderSide: BorderSide(
+              //         color: AppColors.loaderColor,
+              //         width: 1.0,
+              //       ),
+              //     ),
+              //     errorBorder: const OutlineInputBorder(
+              //       borderSide: BorderSide(
+              //         color: Colors.red,
+              //         width: 2,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
+          )
+        ],
       ),
-    );
+    ));
   }
 }
