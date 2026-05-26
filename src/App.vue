0cf85b81b00e65522b7686f675b616397f89d55b
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2026-05-26 22:10:29 +0200

    chore: moved back to flutter

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <RouterView />
-</template>
-
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
-</script>
--- vite.renderer.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import VuePlugin from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config
-export default defineConfig({
-    plugins: [VuePlugin()],
-});
