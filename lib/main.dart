dff03ff03b27ce24346542918921f7b21e7a21b9
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2026-06-03 16:57:16 +0200

    feat: launch full window

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:readit/router/router.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
-  runApp(MaterialApp.router(
-    routerConfig: routerConfig,
-    debugShowCheckedModeBanner: false,
-  ),);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.maximize(); // ← fills the screen edge to edge
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(
+    MaterialApp.router(
+      routerConfig: routerConfig,
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
-
